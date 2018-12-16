c39c26850fad7797bc18eac6b7ef54c8039bddf7
github username <github email address>
2018-12-16 15:52:01 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  state= {
+    people: []
+  }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(json =>
+      this.setState({
+        people: json.people
+      })
+    )
+  }
+
+  render() {
+    let people = this.state.people.map(person => {
+      return <h1> {person.name} </h1>
+    })
+    return (
+      <div>
+        {people}
+      </div>
+    );
+  }
+
+}
+
+export default App;
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Greeting extends Component {
 
   render() {
+
     return (
       <div className="Greeting">
         Hello!
